102e6fb3ded8a4c05acd2f48e521d229c2c707b6
DevanshKarnwal <devansh10.karnwal@gmail.com>
2022-10-03 11:37:15 +0530

    first

---
+++ c11/DSLArray9.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main()
+{
+    int a[50],b[50],c[100],i,j,a1,b1,flag=0,z=0;
+    printf("Enter size of set a ");
+    scanf("%d",&a1);
+    printf("Enter elements of set a \n");
+    for(i=0;i<a1;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Enter size of set b ");
+    scanf("%d",&b1);
+    printf("Enter elements of set b \n");
+    for(i=0;i<b1;i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for(i=0;i<a1;i++)
+    {
+        c[i]=a[i];
+    }
+    z=a1;
+    for(i=0;i<b1;i++)
+    {
+        for(j=0;j<a1;j++)
+        {
+            if(b[i]==a[j])
+                flag=1;
+        }
+        if(flag==0)
+        {
+            c[z]=b[i];
+            z++;
+        }
+        flag=0;
+    }
+    for(i=0;i<z;i++)
+    {
+        printf("%d ",c[i]);
+    }
+}
